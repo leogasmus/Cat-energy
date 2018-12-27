fbaaa42bc3fd1ad064146d81f76b9414409fbde7
leogasmus <leogasmus@gmail.com>
2018-12-27 23:05:51 +0200

    Add responsive navigation add fix some bugs

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,16 @@
-document.getElementById('btnBef').onclick = function(){
-	document.getElementById('line').style.left = "0",
-	document.getElementById('catPhoto').src = 'dist/img/fat-cat.png';
-};
-document.getElementById('btnAf').onclick = function(){
-	document.getElementById('line').style.left = "55px",
-	document.getElementById('catPhoto').src = 'dist/img/tiny-cat.png';	
+window.onload = function(){	
+	var menuBtn = document.querySelector('.toggle'),
+		menuList = document.querySelector('.navigatin__list');
+	menuBtn.onclick = function(){
+		menuBtn.classList.toggle('toggle-close'),
+		menuList.classList.toggle('navigatin__list-hover');
+	};
+	document.querySelector('.control-before').onclick = function(){
+		document.querySelector('.control-line__item').style.left = "0",
+		document.querySelector('.catPhoto').src = 'dist/img/fat-cat.png';
+	};
+	document.querySelector('.control-after').onclick = function(){
+		document.querySelector('.control-line__item').style.left = "55px",
+		document.querySelector('.catPhoto').src = 'dist/img/tiny-cat.png';	
+	};
 };
